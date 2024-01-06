cf3d0539cea846600271355036d78c4a0e0c468b
imanov <teknoradiance@gmail.com>
2024-01-07 02:03:28 +0400

    # database table design

---
+++ bikesystem/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bikesystem
+{
+    internal class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter Sda;
+        private string ConStr;
+
+    }
+}
